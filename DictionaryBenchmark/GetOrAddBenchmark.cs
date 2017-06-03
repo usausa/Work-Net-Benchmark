f8392ea4c9e32da35b5445307518f677b95cee92
usausa <machi.pon@gmail.com>
2017-06-03 19:37:29 +0900

    Add dictionary benchmark

---
+++ DictionaryBenchmark/GetOrAddBenchmark.cs
@@ -0,0 +1,71 @@
+﻿namespace DictionaryBenchmark
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+
+    using BenchmarkDotNet.Attributes;
+
+    public class GetOrAddBenchmark
+    {
+        private const int Loop = 1000;
+
+        [Benchmark]
+        public void Dictionary()
+        {
+            var table = new Dictionary<Type, object>();
+
+            for (var count = 0; count < Loop; count++)
+            {
+                for (var i = 0; i < Classes.Types.Length; i++)
+                {
+                    var key = Classes.Types[i];
+                    if (!table.TryGetValue(key, out object _))
+                    {
+                        table[key] = new object();
+                    }
+                }
+            }
+        }
+
+        [Benchmark]
+        public void DictionaryWithLock()
+        {
+            var sync = new object();
+            var table = new Dictionary<Type, object>();
+
+            for (var count = 0; count < Loop; count++)
+            {
+                for (var i = 0; i < Classes.Types.Length; i++)
+                {
+                    lock (sync)
+                    {
+                        var key = Classes.Types[i];
+                        if (!table.TryGetValue(key, out object _))
+                        {
+                            table[key] = new object();
+                        }
+                    }
+                }
+            }
+        }
+
+        [Benchmark]
+        public void ConcurrentDictionary()
+        {
+            var table = new ConcurrentDictionary<Type, object>();
+
+            for (var count = 0; count < Loop; count++)
+            {
+                for (var i = 0; i < Classes.Types.Length; i++)
+                {
+                    var key = Classes.Types[i];
+                    if (!table.TryGetValue(key, out object _))
+                    {
+                        table[key] = new object();
+                    }
+                }
+            }
+        }
+    }
+}
