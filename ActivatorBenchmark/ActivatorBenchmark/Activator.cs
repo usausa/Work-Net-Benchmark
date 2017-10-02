645cec8affa125f9ecb3fb6be89dc9d339b04961
usausa <machi.pon@gmail.com>
2017-10-02 18:04:58 +0900

    Add IActivator

---
+++ ActivatorBenchmark/ActivatorBenchmark/Activator.cs
@@ -0,0 +1,56 @@
+﻿namespace ActivatorBenchmark
+{
+    using System;
+    using System.Reflection;
+
+    public interface IActivator
+    {
+        object Create(params object[] arguments);
+    }
+
+    public class DelegateActivator : IActivator
+    {
+        private readonly Func<object[], object> func;
+
+        public DelegateActivator(Func<object[], object> func)
+        {
+            this.func = func;
+        }
+
+        public object Create(params object[] arguments)
+        {
+            return func(arguments);
+        }
+    }
+
+    public class ReflectionAcrivatorActivator : IActivator
+    {
+        private readonly Type type;
+
+        public ReflectionAcrivatorActivator(Type type)
+        {
+            this.type = type;
+        }
+
+        public object Create(params object[] arguments)
+        {
+            return Activator.CreateInstance(type, arguments);
+        }
+    }
+
+    public class ReflectionConstructorActivator : IActivator
+    {
+        private readonly ConstructorInfo ci;
+
+        public ReflectionConstructorActivator(ConstructorInfo ci)
+        {
+            this.ci = ci;
+        }
+
+        public object Create(params object[] arguments)
+        {
+            return ci.Invoke(arguments);
+        }
+    }
+
+}
