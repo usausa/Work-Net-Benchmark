9c022bc5acf16657dc1cd06a70fc1ef463133747
usausa <machi.pon@gmail.com>
2018-03-25 14:30:43 +0900

    Add convert benchmark

---
+++ ConvertBenchmark/ConvertBenchmark/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace ConvertBenchmark
+{
+    using System.Reflection;
+    using System.Text;
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Diagnosers;
+    using BenchmarkDotNet.Exporters;
+    using BenchmarkDotNet.Jobs;
+    using BenchmarkDotNet.Running;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).GetTypeInfo().Assembly).Run(args);
+        }
+    }
+
+    public class BenchmarkConfig : ManualConfig
+    {
+        public BenchmarkConfig()
+        {
+            Add(MarkdownExporter.Default, MarkdownExporter.GitHub);
+            Add(MemoryDiagnoser.Default);
+            Add(Job.ShortRun);
+        }
+    }
+
+    [Config(typeof(BenchmarkConfig))]
+    public class EncodingBenchmark
+    {
+        private static readonly byte[] Bytes = { 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39 };
+
+        private Encoding ascii;
+
+        private Encoding utf8;
+
+        private Encoding sjis;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            ascii = Encoding.ASCII;
+            utf8 = Encoding.UTF8;
+            sjis = Encoding.GetEncoding(932);
+        }
+
+        [Benchmark]
+        public void Ascii()
+        {
+            ascii.GetString(Bytes);
+        }
+
+        [Benchmark]
+        public void Utf8()
+        {
+            utf8.GetString(Bytes);
+        }
+
+        [Benchmark]
+        public void Sjis()
+        {
+            sjis.GetString(Bytes);
+        }
+    }
+
+}
---
+++ ConvertBenchmark/README.md
@@ -0,0 +1,9 @@
+# ConvertBenchmark
+
+## Encoding
+
+| Method |     Mean |     Error |    StdDev |  Gen 0 | Allocated |
+|------- |---------:|----------:|----------:|-------:|----------:|
+|  Ascii | 28.33 ns |  1.237 ns | 0.0699 ns | 0.0114 |      48 B |
+|   Utf8 | 38.95 ns |  6.705 ns | 0.3788 ns | 0.0114 |      48 B |
+|   Sjis | 89.61 ns | 70.837 ns | 4.0024 ns | 0.0113 |      48 B |
