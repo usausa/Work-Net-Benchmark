2650fd69cc4adf5df751895bf252fc756d22b1f6
usausa <machi.pon@gmail.com>
2018-05-03 15:14:09 +0900

    Add copy benchmark

---
+++ CopyBenchmark/CopyBenchmark/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace CopyBenchmark
+{
+    using System;
+
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Diagnosers;
+    using BenchmarkDotNet.Exporters;
+    using BenchmarkDotNet.Jobs;
+    using BenchmarkDotNet.Running;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+
+    public class BenchmarkConfig : ManualConfig
+    {
+        public BenchmarkConfig()
+        {
+            Add(MarkdownExporter.Default, MarkdownExporter.GitHub);
+            Add(MemoryDiagnoser.Default);
+            Add(Job.ShortRun);
+        }
+    }
+
+    [Config(typeof(BenchmarkConfig))]
+    public class Benchmark
+    {
+        private byte[] source;
+
+        private byte[] destination;
+
+        [Params(4, 32, 64, 128)]
+        public int Length { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            source = new byte[Length];
+            destination = new byte[Length];
+        }
+
+        [Benchmark]
+        public void ArrayCopy()
+        {
+            Array.Copy(source, 0, destination, 0, source.Length);
+        }
+
+        [Benchmark]
+        public void BlockCopy()
+        {
+            Buffer.BlockCopy(source, 0, destination, 0, source.Length);
+        }
+
+        [Benchmark]
+        public unsafe void MemoryCopy()
+        {
+            fixed (byte* pSource = &source[0])
+            fixed (byte* pDestination = &destination[0])
+            {
+                Buffer.MemoryCopy(pSource, pDestination, destination.Length, source.Length);
+            }
+        }
+    }
+}
---
+++ CopyBenchmark/README.md
@@ -0,0 +1,14 @@
+|     Method | Length |      Mean |     Error |    StdDev | Allocated |
+|----------- |------- |----------:|----------:|----------:|----------:|
+|  **ArrayCopy** |      **4** | **23.303 ns** | **12.410 ns** | **0.7012 ns** |       **0 B** |
+|  BlockCopy |      4 |  9.431 ns |  5.949 ns | 0.3361 ns |       0 B |
+| MemoryCopy |      4 |  4.182 ns |  1.128 ns | 0.0637 ns |       0 B |
+|  **ArrayCopy** |     **32** | **21.919 ns** |  **7.738 ns** | **0.4372 ns** |       **0 B** |
+|  BlockCopy |     32 |  7.704 ns |  3.524 ns | 0.1991 ns |       0 B |
+| MemoryCopy |     32 |  3.758 ns |  2.494 ns | 0.1409 ns |       0 B |
+|  **ArrayCopy** |     **64** | **20.154 ns** |  **2.053 ns** | **0.1160 ns** |       **0 B** |
+|  BlockCopy |     64 |  8.124 ns |  3.028 ns | 0.1711 ns |       0 B |
+| MemoryCopy |     64 |  4.712 ns |  2.558 ns | 0.1445 ns |       0 B |
+|  **ArrayCopy** |    **128** | **21.484 ns** |  **7.134 ns** | **0.4031 ns** |       **0 B** |
+|  BlockCopy |    128 | 11.042 ns |  2.383 ns | 0.1346 ns |       0 B |
+| MemoryCopy |    128 |  6.730 ns |  2.643 ns | 0.1494 ns |       0 B |
