694e3c1332111dbec90fa3a86c5b6d532ad795e2
usausa <machi.pon@gmail.com>
2018-05-18 09:40:08 +0900

    Add switch benchmark

---
+++ SwitchBenchmark/README.md
@@ -0,0 +1,20 @@
+|          Method | Parameter |      Mean |     Error |    StdDev | Allocated |
+|---------------- |---------- |----------:|----------:|----------:|----------:|
+|        **IfStatic** |         **1** | **0.3511 ns** | **1.2804 ns** | **0.0723 ns** |       **0 B** |
+|  InstanceAction |         1 | 1.0553 ns | 0.5497 ns | 0.0311 ns |       0 B |
+| InstanceAction2 |         1 | 2.6289 ns | 1.4559 ns | 0.0823 ns |       0 B |
+|    StaticAction |         1 | 1.0859 ns | 0.2137 ns | 0.0121 ns |       0 B |
+|   StaticAction2 |         1 | 2.5185 ns | 0.2865 ns | 0.0162 ns |       0 B |
+| InterfaceAction |         1 | 1.3152 ns | 0.2880 ns | 0.0163 ns |       0 B |
+|        **IfStatic** |         **2** | **0.2641 ns** | **0.0920 ns** | **0.0052 ns** |       **0 B** |
+|  InstanceAction |         2 | 1.0232 ns | 0.1436 ns | 0.0081 ns |       0 B |
+| InstanceAction2 |         2 | 2.4225 ns | 0.6903 ns | 0.0390 ns |       0 B |
+|    StaticAction |         2 | 0.9767 ns | 0.1749 ns | 0.0099 ns |       0 B |
+|   StaticAction2 |         2 | 2.4324 ns | 0.2918 ns | 0.0165 ns |       0 B |
+| InterfaceAction |         2 | 1.3177 ns | 0.1249 ns | 0.0071 ns |       0 B |
+|        **IfStatic** |         **3** | **0.8572 ns** | **0.2680 ns** | **0.0151 ns** |       **0 B** |
+|  InstanceAction |         3 | 1.0195 ns | 0.2816 ns | 0.0159 ns |       0 B |
+| InstanceAction2 |         3 | 2.4196 ns | 0.4223 ns | 0.0239 ns |       0 B |
+|    StaticAction |         3 | 1.1359 ns | 6.4592 ns | 0.3650 ns |       0 B |
+|   StaticAction2 |         3 | 2.4546 ns | 1.0897 ns | 0.0616 ns |       0 B |
+| InterfaceAction |         3 | 1.3121 ns | 0.1931 ns | 0.0109 ns |       0 B |
---
+++ SwitchBenchmark/SwitchBenchmark/Program.cs
@@ -0,0 +1,186 @@
+﻿namespace SwitchBenchmark
+{
+    using System;
+    using System.Runtime.CompilerServices;
+
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Diagnosers;
+    using BenchmarkDotNet.Exporters;
+    using BenchmarkDotNet.Jobs;
+    using BenchmarkDotNet.Running;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+
+    public class BenchmarkConfig : ManualConfig
+    {
+        public BenchmarkConfig()
+        {
+            Add(MarkdownExporter.Default, MarkdownExporter.GitHub);
+            Add(MemoryDiagnoser.Default);
+            Add(Job.ShortRun);
+        }
+    }
+
+    [Config(typeof(BenchmarkConfig))]
+    public class Benchmark
+    {
+        private Action<int> instanceAction;
+
+        private Action<int> instanceAction2;
+
+        private Action<int> staticAction;
+
+        private Action<int> staticAction2;
+
+        private IAction interfaceAction;
+
+        [Params(1, 2, 3)]
+        public int Parameter { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            if (Parameter == 1)
+            {
+                instanceAction = Action1.Default.Work;
+                instanceAction2 = x => Action1.Default.Work(1);
+                staticAction = Action1.Default.Work;
+                staticAction2 = x => Action1.Default.Work(1);
+                interfaceAction = Action1.Default;
+            }
+            else if (Parameter == 2)
+            {
+                instanceAction = Action2.Default.Work;
+                instanceAction2 = x => Action2.Default.Work(2);
+                staticAction = Action2.Default.Work;
+                staticAction2 = x => Action2.Default.Work(2);
+                interfaceAction = Action2.Default;
+            }
+            else if (Parameter == 3)
+            {
+                instanceAction = Action3.Default.Work;
+                instanceAction2 = x => Action3.Default.Work(3);
+                staticAction = Action3.Default.Work;
+                staticAction2 = x => Action3.Default.Work(3);
+                interfaceAction = Action3.Default;
+            }
+        }
+
+        [Benchmark]
+        public void IfStatic()
+        {
+            if (Parameter == 1)
+            {
+                Data.Value = 1;
+            }
+            else if (Parameter == 2)
+            {
+                Data.Value = 2;
+            }
+            else if (Parameter == 3)
+            {
+                Data.Value = 3;
+            }
+        }
+
+        [Benchmark]
+        public void InstanceAction()
+        {
+            instanceAction(Parameter);
+        }
+
+
+        [Benchmark]
+        public void InstanceAction2()
+        {
+            instanceAction2(Parameter);
+        }
+
+        [Benchmark]
+        public void StaticAction()
+        {
+            staticAction(Parameter);
+        }
+
+
+        [Benchmark]
+        public void StaticAction2()
+        {
+            staticAction2(Parameter);
+        }
+
+        [Benchmark]
+        public void InterfaceAction()
+        {
+            interfaceAction.Work(Parameter);
+        }
+    }
+
+    public static class Data
+    {
+        public static int Value { get; set; }
+    }
+
+    public interface IAction
+    {
+        void Work(int parameter);
+    }
+
+    public sealed class Action1 : IAction
+    {
+        public static IAction Default { get; } = new Action1();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Work(int parameter)
+        {
+            Data.Value = parameter;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WorkStatic(int parameter)
+        {
+            Data.Value = parameter;
+        }
+    }
+
+    public sealed class Action2 : IAction
+    {
+        public static IAction Default { get; } = new Action2();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Work(int parameter)
+        {
+            Data.Value = parameter;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WorkStatic(int parameter)
+        {
+            Data.Value = parameter;
+        }
+    }
+
+    public sealed class Action3 : IAction
+    {
+        public static IAction Default { get; } = new Action3();
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Work(int parameter)
+        {
+            Data.Value = parameter;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WorkStatic(int parameter)
+        {
+            Data.Value = parameter;
+        }
+    }
+}
