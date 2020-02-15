f3f9b5dd93f1eee1f3378fac9edf08e60e48a2e4
usausa <machi.pon@gmail.com>
2020-02-15 11:42:19 +0900

    Update

---
+++ Benchmarks/Benchmarks/Classes.cs
@@ -0,0 +1,212 @@
+﻿namespace Benchmarks
+{
+    using System;
+
+    public class Class00 { }
+    public class Class01 { }
+    public class Class02 { }
+    public class Class03 { }
+    public class Class04 { }
+    public class Class05 { }
+    public class Class06 { }
+    public class Class07 { }
+    public class Class08 { }
+    public class Class09 { }
+    public class Class10 { }
+    public class Class11 { }
+    public class Class12 { }
+    public class Class13 { }
+    public class Class14 { }
+    public class Class15 { }
+    public class Class16 { }
+    public class Class17 { }
+    public class Class18 { }
+    public class Class19 { }
+    public class Class20 { }
+    public class Class21 { }
+    public class Class22 { }
+    public class Class23 { }
+    public class Class24 { }
+    public class Class25 { }
+    public class Class26 { }
+    public class Class27 { }
+    public class Class28 { }
+    public class Class29 { }
+    public class Class30 { }
+    public class Class31 { }
+    public class Class32 { }
+    public class Class33 { }
+    public class Class34 { }
+    public class Class35 { }
+    public class Class36 { }
+    public class Class37 { }
+    public class Class38 { }
+    public class Class39 { }
+    public class Class40 { }
+    public class Class41 { }
+    public class Class42 { }
+    public class Class43 { }
+    public class Class44 { }
+    public class Class45 { }
+    public class Class46 { }
+    public class Class47 { }
+    public class Class48 { }
+    public class Class49 { }
+    public class Class50 { }
+    public class Class51 { }
+    public class Class52 { }
+    public class Class53 { }
+    public class Class54 { }
+    public class Class55 { }
+    public class Class56 { }
+    public class Class57 { }
+    public class Class58 { }
+    public class Class59 { }
+    public class Class60 { }
+    public class Class61 { }
+    public class Class62 { }
+    public class Class63 { }
+    public class Class64 { }
+    public class Class65 { }
+    public class Class66 { }
+    public class Class67 { }
+    public class Class68 { }
+    public class Class69 { }
+    public class Class70 { }
+    public class Class71 { }
+    public class Class72 { }
+    public class Class73 { }
+    public class Class74 { }
+    public class Class75 { }
+    public class Class76 { }
+    public class Class77 { }
+    public class Class78 { }
+    public class Class79 { }
+    public class Class80 { }
+    public class Class81 { }
+    public class Class82 { }
+    public class Class83 { }
+    public class Class84 { }
+    public class Class85 { }
+    public class Class86 { }
+    public class Class87 { }
+    public class Class88 { }
+    public class Class89 { }
+    public class Class90 { }
+    public class Class91 { }
+    public class Class92 { }
+    public class Class93 { }
+    public class Class94 { }
+    public class Class95 { }
+    public class Class96 { }
+    public class Class97 { }
+    public class Class98 { }
+    public class Class99 { }
+
+    public static class Classes
+    {
+        public static Type[] Types => new[]
+        {
+            typeof(Class00),
+            typeof(Class01),
+            typeof(Class02),
+            typeof(Class03),
+            typeof(Class04),
+            typeof(Class05),
+            typeof(Class06),
+            typeof(Class07),
+            typeof(Class08),
+            typeof(Class09),
+            typeof(Class10),
+            typeof(Class11),
+            typeof(Class12),
+            typeof(Class13),
+            typeof(Class14),
+            typeof(Class15),
+            typeof(Class16),
+            typeof(Class17),
+            typeof(Class18),
+            typeof(Class19),
+            typeof(Class20),
+            typeof(Class21),
+            typeof(Class22),
+            typeof(Class23),
+            typeof(Class24),
+            typeof(Class25),
+            typeof(Class26),
+            typeof(Class27),
+            typeof(Class28),
+            typeof(Class29),
+            typeof(Class30),
+            typeof(Class31),
+            typeof(Class32),
+            typeof(Class33),
+            typeof(Class34),
+            typeof(Class35),
+            typeof(Class36),
+            typeof(Class37),
+            typeof(Class38),
+            typeof(Class39),
+            typeof(Class40),
+            typeof(Class41),
+            typeof(Class42),
+            typeof(Class43),
+            typeof(Class44),
+            typeof(Class45),
+            typeof(Class46),
+            typeof(Class47),
+            typeof(Class48),
+            typeof(Class49),
+            typeof(Class50),
+            typeof(Class51),
+            typeof(Class52),
+            typeof(Class53),
+            typeof(Class54),
+            typeof(Class55),
+            typeof(Class56),
+            typeof(Class57),
+            typeof(Class58),
+            typeof(Class59),
+            typeof(Class60),
+            typeof(Class61),
+            typeof(Class62),
+            typeof(Class63),
+            typeof(Class64),
+            typeof(Class65),
+            typeof(Class66),
+            typeof(Class67),
+            typeof(Class68),
+            typeof(Class69),
+            typeof(Class70),
+            typeof(Class71),
+            typeof(Class72),
+            typeof(Class73),
+            typeof(Class74),
+            typeof(Class75),
+            typeof(Class76),
+            typeof(Class77),
+            typeof(Class78),
+            typeof(Class79),
+            typeof(Class80),
+            typeof(Class81),
+            typeof(Class82),
+            typeof(Class83),
+            typeof(Class84),
+            typeof(Class85),
+            typeof(Class86),
+            typeof(Class87),
+            typeof(Class88),
+            typeof(Class89),
+            typeof(Class90),
+            typeof(Class91),
+            typeof(Class92),
+            typeof(Class93),
+            typeof(Class94),
+            typeof(Class95),
+            typeof(Class96),
+            typeof(Class97),
+            typeof(Class98),
+            typeof(Class99)
+        };
+    }
+}
---
+++ Benchmarks/Benchmarks/KeyPairHash/KeyPairHashBenchmark.cs
@@ -0,0 +1,115 @@
+﻿namespace Benchmarks.KeyPairHash
+{
+    using System;
+    using System.Linq;
+
+    using BenchmarkDotNet.Attributes;
+
+    [Config(typeof(BenchmarkConfig))]
+    public class KeyPairHashBenchmark
+    {
+        private readonly StructPair<Type, Type>[] typePairKeys = Enumerable.Range(1, 10000)
+            .Select(x => new StructPair<Type, Type>(Classes.Types[x / 100], Classes.Types[x % 100]))
+            .ToArray();
+
+        private readonly StructPair<Type, string>[] typeStringKeys = Enumerable.Range(1, 10000)
+            .Select(x => new StructPair<Type, string>(Classes.Types[x / 100], $"{x % 100}:D3"))
+            .ToArray();
+
+        [Benchmark(OperationsPerInvoke = 100 * 100)]
+        public int TypePairXorWithoutLocal()
+        {
+            var ret = 0;
+            for (var i = 0; i < typePairKeys.Length; i++)
+            {
+                ret = typePairKeys[i].Key1.GetHashCode() ^ typePairKeys[i].Key2.GetHashCode();
+            }
+            return ret;
+        }
+
+        [Benchmark(OperationsPerInvoke = 100 * 100)]
+        public int TypePairXor()
+        {
+            var ret = 0;
+            for (var i = 0; i < typePairKeys.Length; i++)
+            {
+                var pair = typePairKeys[i];
+                ret = pair.Key1.GetHashCode() ^ pair.Key2.GetHashCode();
+            }
+            return ret;
+        }
+
+        [Benchmark(OperationsPerInvoke = 100 * 100)]
+        public int TypePairXor397()
+        {
+            var ret = 0;
+            for (var i = 0; i < typePairKeys.Length; i++)
+            {
+                var pair = typePairKeys[i];
+                ret = (pair.Key1.GetHashCode() * 397) ^ pair.Key2.GetHashCode();
+            }
+            return ret;
+        }
+
+        [Benchmark(OperationsPerInvoke = 100 * 100)]
+        public int TypePairValueTuple()
+        {
+            var ret = 0;
+            for (var i = 0; i < typePairKeys.Length; i++)
+            {
+                var pair = typePairKeys[i];
+                ret = ValueTuple.Create(pair.Key1, pair.Key2).GetHashCode();
+            }
+            return ret;
+        }
+
+        [Benchmark(OperationsPerInvoke = 100 * 100)]
+        public int TypeStringXor()
+        {
+            var ret = 0;
+            for (var i = 0; i < typeStringKeys.Length; i++)
+            {
+                var pair = typeStringKeys[i];
+                ret = pair.Key1.GetHashCode() ^ pair.Key2.GetHashCode();
+            }
+            return ret;
+        }
+
+        [Benchmark(OperationsPerInvoke = 100 * 100)]
+        public int TypeStringXor397()
+        {
+            var ret = 0;
+            for (var i = 0; i < typeStringKeys.Length; i++)
+            {
+                var pair = typeStringKeys[i];
+                ret = (pair.Key1.GetHashCode() * 397) ^ pair.Key2.GetHashCode();
+            }
+            return ret;
+        }
+
+        [Benchmark(OperationsPerInvoke = 100 * 100)]
+        public int TypeStringValueTuple()
+        {
+            var ret = 0;
+            for (var i = 0; i < typeStringKeys.Length; i++)
+            {
+                var pair = typeStringKeys[i];
+                ret = ValueTuple.Create(pair.Key1, pair.Key2).GetHashCode();
+            }
+            return ret;
+        }
+    }
+
+    public struct StructPair<T1, T2>
+    {
+        public readonly T1 Key1;
+
+        public readonly T2 Key2;
+
+        public StructPair(T1 key1, T2 key2)
+        {
+            Key1 = key1;
+            Key2 = key2;
+        }
+    }
+}
