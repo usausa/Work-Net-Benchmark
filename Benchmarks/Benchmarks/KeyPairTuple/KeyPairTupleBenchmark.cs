204acfe01ca520a3e1c151f69b9d38900fa463ca
usausa <machi.pon@gmail.com>
2020-02-15 11:01:53 +0900

    Update benchmark

---
+++ Benchmarks/Benchmarks/KeyPairTuple/KeyPairTupleBenchmark.cs
@@ -0,0 +1,90 @@
+﻿namespace Benchmarks.KeyPairTuple
+{
+    using System;
+
+    using BenchmarkDotNet.Attributes;
+
+    [Config(typeof(BenchmarkConfig))]
+    public class KeyPairTupleBenchmark
+    {
+        private const int N = 1000;
+
+        private readonly ClassFieldKey classFieldKey = new ClassFieldKey(typeof(KeyPairTupleBenchmark), "Test");
+
+        private readonly ClassPropertyKey classPropertyKey = new ClassPropertyKey(typeof(KeyPairTupleBenchmark), "Test");
+
+        private readonly StructKey structKey = new StructKey(typeof(KeyPairTupleBenchmark), "Test");
+
+        [Benchmark(OperationsPerInvoke = N, Baseline = true)]
+        public int UseClassField()
+        {
+            var ret = 0;
+            for (var i = 0; i < N; i++)
+            {
+                ret = classFieldKey.Type.GetHashCode() ^ (classFieldKey.Name.GetHashCode() * 397);
+            }
+            return ret;
+        }
+
+        [Benchmark(OperationsPerInvoke = N)]
+        public int UseClassProperty()
+        {
+            var ret = 0;
+            for (var i = 0; i < N; i++)
+            {
+                ret = classPropertyKey.Type.GetHashCode() ^ (classPropertyKey.Name.GetHashCode() * 397);
+            }
+            return ret;
+        }
+
+        [Benchmark(OperationsPerInvoke = N)]
+        public int UseStruct()
+        {
+            var ret = 0;
+            for (var i = 0; i < N; i++)
+            {
+                ret = structKey.Type.GetHashCode() ^ (structKey.Name.GetHashCode() * 397);
+            }
+            return ret;
+        }
+    }
+
+    public class ClassFieldKey
+    {
+        public readonly Type Type;
+
+        public readonly string Name;
+
+        public ClassFieldKey(Type type, string name)
+        {
+            Type = type;
+            Name = name;
+        }
+    }
+
+    public class ClassPropertyKey
+    {
+        public Type Type { get; }
+
+        public string Name { get; }
+
+        public ClassPropertyKey(Type type, string name)
+        {
+            Type = type;
+            Name = name;
+        }
+    }
+
+    public struct StructKey
+    {
+        public readonly Type Type;
+
+        public readonly string Name;
+
+        public StructKey(Type type, string name)
+        {
+            Type = type;
+            Name = name;
+        }
+    }
+}
