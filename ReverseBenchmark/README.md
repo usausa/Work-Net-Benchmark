846206886a80014703c529d31155817495197c5a
usausa <machi.pon@gmail.com>
2020-11-23 12:39:24 +0900

    Add reverse benchmark

---
+++ ReverseBenchmark/README.md
@@ -0,0 +1,26 @@
+|                Method |       Mean |     Error |    StdDev | Gen 0 | Gen 1 | Gen 2 | Allocated |
+|---------------------- |-----------:|----------:|----------:|------:|------:|------:|----------:|
+|           ReverseInt4 |   4.503 ns | 0.0259 ns | 0.0371 ns |     - |     - |     - |         - |
+|     ReverseUnsafeInt4 |   3.347 ns | 0.0417 ns | 0.0598 ns |     - |     - |     - |         - |
+|    ReversePointerInt4 |   3.772 ns | 0.0386 ns | 0.0566 ns |     - |     - |     - |         - |
+|           ReverseInt8 |   7.443 ns | 0.1762 ns | 0.2471 ns |     - |     - |     - |         - |
+|     ReverseUnsafeInt8 |   5.625 ns | 0.0414 ns | 0.0620 ns |     - |     - |     - |         - |
+|    ReversePointerInt8 |   5.670 ns | 0.0509 ns | 0.0730 ns |     - |     - |     - |         - |
+|          ReverseInt32 |  20.013 ns | 0.2857 ns | 0.4277 ns |     - |     - |     - |         - |
+|    ReverseUnsafeInt32 |  15.510 ns | 0.2128 ns | 0.3119 ns |     - |     - |     - |         - |
+|   ReversePointerInt32 |  15.909 ns | 0.1747 ns | 0.2614 ns |     - |     - |     - |         - |
+|         ReverseInt256 | 141.640 ns | 1.4839 ns | 2.1750 ns |     - |     - |     - |         - |
+|   ReverseUnsafeInt256 | 109.237 ns | 0.6951 ns | 1.0188 ns |     - |     - |     - |         - |
+|  ReversePointerInt256 | 104.540 ns | 0.6142 ns | 0.8809 ns |     - |     - |     - |         - |
+|          ReverseByte4 |   4.538 ns | 0.0415 ns | 0.0582 ns |     - |     - |     - |         - |
+|    ReverseUnsafeByte4 |   3.329 ns | 0.0330 ns | 0.0463 ns |     - |     - |     - |         - |
+|   ReversePointerByte4 |   3.810 ns | 0.0324 ns | 0.0475 ns |     - |     - |     - |         - |
+|          ReverseByte8 |   7.200 ns | 0.0482 ns | 0.0722 ns |     - |     - |     - |         - |
+|    ReverseUnsafeByte8 |   5.694 ns | 0.0482 ns | 0.0722 ns |     - |     - |     - |         - |
+|   ReversePointerByte8 |   5.825 ns | 0.0478 ns | 0.0685 ns |     - |     - |     - |         - |
+|         ReverseByte32 |  19.913 ns | 0.2829 ns | 0.4147 ns |     - |     - |     - |         - |
+|   ReverseUnsafeByte32 |  15.297 ns | 0.2426 ns | 0.3321 ns |     - |     - |     - |         - |
+|  ReversePointerByte32 |  15.505 ns | 0.0980 ns | 0.1373 ns |     - |     - |     - |         - |
+|        ReverseByte256 | 139.850 ns | 0.6725 ns | 1.0065 ns |     - |     - |     - |         - |
+|  ReverseUnsafeByte256 | 109.083 ns | 0.4654 ns | 0.6822 ns |     - |     - |     - |         - |
+| ReversePointerByte256 | 104.330 ns | 0.4003 ns | 0.5991 ns |     - |     - |     - |         - |
---
+++ ReverseBenchmark/ReverseBenchmark/Program.cs
@@ -0,0 +1,173 @@
+﻿namespace ReverseBenchmark
+{
+    using System;
+    using System.Runtime.CompilerServices;
+    using System.Runtime.InteropServices;
+    using System.Linq;
+
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Configs;
+    using BenchmarkDotNet.Diagnosers;
+    using BenchmarkDotNet.Exporters;
+    using BenchmarkDotNet.Jobs;
+    using BenchmarkDotNet.Running;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+
+    public class BenchmarkConfig : ManualConfig
+    {
+        public BenchmarkConfig()
+        {
+            AddExporter(MarkdownExporter.Default, MarkdownExporter.GitHub);
+            AddDiagnoser(MemoryDiagnoser.Default);
+            AddJob(Job.MediumRun);
+        }
+    }
+
+    [Config(typeof(BenchmarkConfig))]
+    public class Benchmark
+    {
+        private readonly int[] intArray = Enumerable.Range(0, 256).ToArray();
+        private readonly byte[] byteArray = Enumerable.Range(0, 256).Select(x => (byte)x).ToArray();
+
+        [Benchmark]
+        public void ReverseInt4() => ReverseOperator.Reverse(intArray.AsSpan(0, 4));
+
+        [Benchmark]
+        public void ReverseUnsafeInt4() => ReverseOperator.ReverseUnsafe(intArray.AsSpan(0, 4));
+
+        [Benchmark]
+        public void ReversePointerInt4() => ReverseOperator.ReversePointer(intArray.AsSpan(0, 4));
+
+        [Benchmark]
+        public void ReverseInt8() => ReverseOperator.Reverse(intArray.AsSpan(0, 8));
+
+        [Benchmark]
+        public void ReverseUnsafeInt8() => ReverseOperator.ReverseUnsafe(intArray.AsSpan(0, 8));
+
+        [Benchmark]
+        public void ReversePointerInt8() => ReverseOperator.ReversePointer(intArray.AsSpan(0, 8));
+
+        [Benchmark]
+        public void ReverseInt32() => ReverseOperator.Reverse(intArray.AsSpan(0, 32));
+
+        [Benchmark]
+        public void ReverseUnsafeInt32() => ReverseOperator.ReverseUnsafe(intArray.AsSpan(0, 32));
+
+        [Benchmark]
+        public void ReversePointerInt32() => ReverseOperator.ReversePointer(intArray.AsSpan(0, 32));
+
+        [Benchmark]
+        public void ReverseInt256() => ReverseOperator.Reverse(intArray.AsSpan(0, 256));
+
+        [Benchmark]
+        public void ReverseUnsafeInt256() => ReverseOperator.ReverseUnsafe(intArray.AsSpan(0, 256));
+
+        [Benchmark]
+        public void ReversePointerInt256() => ReverseOperator.ReversePointer(intArray.AsSpan(0, 256));
+
+        [Benchmark]
+        public void ReverseByte4() => ReverseOperator.Reverse(byteArray.AsSpan(0, 4));
+
+        [Benchmark]
+        public void ReverseUnsafeByte4() => ReverseOperator.ReverseUnsafe(byteArray.AsSpan(0, 4));
+
+        [Benchmark]
+        public void ReversePointerByte4() => ReverseOperator.ReversePointer(byteArray.AsSpan(0, 4));
+
+        [Benchmark]
+        public void ReverseByte8() => ReverseOperator.Reverse(byteArray.AsSpan(0, 8));
+
+        [Benchmark]
+        public void ReverseUnsafeByte8() => ReverseOperator.ReverseUnsafe(byteArray.AsSpan(0, 8));
+
+        [Benchmark]
+        public void ReversePointerByte8() => ReverseOperator.ReversePointer(byteArray.AsSpan(0, 8));
+
+        [Benchmark]
+        public void ReverseByte32() => ReverseOperator.Reverse(byteArray.AsSpan(0, 32));
+
+        [Benchmark]
+        public void ReverseUnsafeByte32() => ReverseOperator.ReverseUnsafe(byteArray.AsSpan(0, 32));
+
+        [Benchmark]
+        public void ReversePointerByte32() => ReverseOperator.ReversePointer(byteArray.AsSpan(0, 32));
+
+        [Benchmark]
+        public void ReverseByte256() => ReverseOperator.Reverse(byteArray.AsSpan(0, 256));
+
+        [Benchmark]
+        public void ReverseUnsafeByte256() => ReverseOperator.ReverseUnsafe(byteArray.AsSpan(0, 256));
+
+        [Benchmark]
+        public void ReversePointerByte256() => ReverseOperator.ReversePointer(byteArray.AsSpan(0, 256));
+    }
+
+    public static class ReverseOperator
+    {
+        public static void Reverse<T>(Span<T> array)
+        {
+            for (var i = 0; i < array.Length >> 1; i++)
+            {
+                T temp = array[i];
+                array[i] = array[array.Length - i - 1];
+                array[array.Length - i - 1] = temp;
+            }
+        }
+
+        public static void ReverseUnsafe<T>(Span<T> array)
+        {
+            ref T p = ref MemoryMarshal.GetReference(array);
+            var i = 0;
+            var j = array.Length - 1;
+            while (i < j)
+            {
+                T temp = Unsafe.Add(ref p, i);
+                Unsafe.Add(ref p, i) = Unsafe.Add(ref p, j);
+                Unsafe.Add(ref p, j) = temp;
+                i++;
+                j--;
+            }
+        }
+
+        public static unsafe void ReversePointer(Span<byte> array)
+        {
+            var i = 0;
+            var j = array.Length - 1;
+            fixed (byte* p = array)
+            {
+                while (i < j)
+                {
+                    var temp = *(p + i);
+                    *(p + i) = *(p + j);
+                    *(p + j) = temp;
+                    i++;
+                    j--;
+                }
+            }
+        }
+
+        public static unsafe void ReversePointer(Span<int> array)
+        {
+            var i = 0;
+            var j = array.Length - 1;
+            fixed (int* p = array)
+            {
+                while (i < j)
+                {
+                    var temp = *(p + i);
+                    *(p + i) = *(p + j);
+                    *(p + j) = temp;
+                    i++;
+                    j--;
+                }
+            }
+        }
+    }
+}
