9e1ad4e68b4dbd911626cf2247d689568630ce76
usausa <machi.pon@gmail.com>
2025-10-28 10:24:07 +0900

    Update

--- BasicBenchmark/Program.cs
+++ BasicBenchmark/Program.cs
@@ -6,6 +6,7 @@ using BenchmarkDotNet.Columns;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 
 public static class Program
@@ -38,6 +39,8 @@ public delegate void CustomDelegate(EventArgs args);
 
 #pragma warning disable CA1822
 [Config(typeof(BenchmarkConfig))]
+[MediumRunJob(RuntimeMoniker.Net90)]
+[MediumRunJob(RuntimeMoniker.Net10_0)]
 public class Benchmark
 {
     private const int N = 1_000_000;
